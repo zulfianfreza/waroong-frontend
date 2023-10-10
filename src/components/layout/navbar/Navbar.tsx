92c8d4140b35be6cbbd683f292a1d53255217bf4
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2023-10-10 20:51:32 +0700

    init tailwind and add google fonts

---
+++ src/components/layout/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="">
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      display: ["Comfortaa", "cursive"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
